9b55eeb8f58f704ef98d9ceddab429e08829a254
Karina Garces <kgarces8@gmail.com>
2021-01-26 22:50:48 -0500

    Committing iterative statement

--- IterativeStatements/Program.cs
+++ IterativeStatements/Program.cs
@@ -6,7 +6,14 @@ namespace IterativeStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer value between 1 and 100");
+            int value = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < value; i++)
+            {
+                Console.WriteLine($"You have entered {value}. This is the current integer value in the loop: {i + 1}");
+            }
+            Console.ReadKey(true);
         }
     }
 }
